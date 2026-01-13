a3bd25ff5f3711613fd1c3c1e3d677c70265ed2d
jashnavi1718-cloud <jashnavi1718@gmail.com>
2026-01-13 15:37:15 +0530

    Add grading system based on percentage input

---
+++ day3_conditions.py
@@ -0,0 +1,10 @@
+percentage = float(input("Enter your percentage: "))
+
+if percentage >= 90:
+    print("Grade A")
+elif percentage >= 75:
+    print("Grade B")
+elif percentage >= 60:
+    print("Grade C")
+else:
+    print("Fail")
